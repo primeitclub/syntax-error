1fec67701af43c865026c6800dfc81db467457fd
pypravin <pravingyawali.study@gmail.com>
2025-06-14 11:01:23 +0545

    Added - requirement.txt

--- Net2Connect/Accounts/models.py
+++ Net2Connect/Accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+def create_user():
+    user = User.objects.create_user("admin","Pravin.admin@gmail.com","admin123")
+    user.first_name='Pravin',
+    user.last_name = "Gyawali"
+
+    user.save()
--- Net2Connect/Accounts/views.py
+++ Net2Connect/Accounts/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return render(request, 'home.html', {'user': user})
+        else:
+            return render(request, 'login.html', {'error': 'Invalid credentials'})
+    
+    return render(request, 'login.html')
+
+# Logout view
+def logout_view(request):
+    logout(request)
+    return render(request, 'login.html', {'message': 'You have been logged out.'})
