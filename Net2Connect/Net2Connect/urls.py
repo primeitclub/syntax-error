9dd7244c6256820d5a666c80c0e52c3d3b09ebd0
Prasid <prasidshahi5@gmail.com>
2025-06-14 11:20:06 +0545

    dashboard push for sugen

--- Net2Connect/Net2Connect/urls.py
+++ Net2Connect/Net2Connect/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Dashboard import views
+
 
 urlpatterns = [
+    path('dashboard/',views.dashboard, name='dashboard'),
+
     path('admin/', admin.site.urls),
 ]
