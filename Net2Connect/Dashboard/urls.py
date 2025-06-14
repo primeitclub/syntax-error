d7a985363cc03507fa9203857895d74d8e9a9be0
genzegen <genzahm.lincoln@gmail.com>
2025-06-14 16:00:06 +0545

    collab page

--- Net2Connect/Dashboard/urls.py
+++ Net2Connect/Dashboard/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('dashboard/',views.dashboard,name="dashboard"),
     path('feed/',views.feed,name="feed"),
     path('group/',views.group,name="group"),
-    path('collab/',views.dashboard,name="collab"),
-    path('inbox/',views.dashboard,name="inbox"),
-    path('notification/',views.dashboard,name="notification"),
+    path('collab/',views.collab,name="collab"),
+    path('inbox/',views.inbox,name="inbox"),
+    path('notification/',views.notification,name="notification"),
 ]
